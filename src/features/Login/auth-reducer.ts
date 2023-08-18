bc8457bca6883e49c19cda5777ca70603f40519a
Ustin4 <Mortyvp@yandex.by>
2023-08-18 22:09:48 +0300

    add Petrick and setting

--- src/features/Login/auth-reducer.ts
+++ src/features/Login/auth-reducer.ts
@@ -1,58 +1,59 @@
-import { authAPI, LoginParamsType } from "api/todolists-api"
-import { handleServerAppError, handleServerNetworkError } from "utils/error-utils"
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { AppThunk } from "app/store"
-import { appActions } from "app/app-reducer"
+import { authAPI, LoginParamsType } from 'api/todolists-api';
+import { handleServerAppError, handleServerNetworkError } from 'utils/error-utils';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { AppThunk } from 'app/store';
+import { appActions } from 'app/app-reducer';
 
 const slice = createSlice({
-  name: "auth",
+  name: 'auth',
   initialState: {
-    isLoggedIn: false,
+    isLoggedIn: false
   },
   reducers: {
     setIsLoggedIn: (state, action: PayloadAction<{ isLoggedIn: boolean }>) => {
-      state.isLoggedIn = action.payload.isLoggedIn
-    },
-  },
-})
+      state.isLoggedIn = action.payload.isLoggedIn;
+    }
+  }
+});
 
-export const authActions = slice.actions
-export const authReducer = slice.reducer
+export const authActions = slice.actions;
+export const authReducer = slice.reducer;
 
 export const loginTC =
   (data: LoginParamsType): AppThunk =>
-  (dispatch) => {
-    dispatch(appActions.setAppStatus({ status: "loading" }))
-    authAPI
-      .login(data)
-      .then((res) => {
-        if (res.data.resultCode === 0) {
-          // dispatch(setIsLoggedInAC(true))
-          dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }))
-          dispatch(appActions.setAppStatus({ status: "succeeded" }))
-        } else {
-          handleServerAppError(res.data, dispatch)
-        }
-      })
-      .catch((error) => {
-        handleServerNetworkError(error, dispatch)
-      })
-  }
+    (dispatch) => {
+      dispatch(appActions.setAppStatus({ status: 'loading' }));
+      authAPI
+        .login(data)
+        .then((res) => {
+          if (res.data.resultCode === 0) {
+            // dispatch(setIsLoggedInAC(true))
+            dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }));
+            dispatch(appActions.setAppStatus({ status: 'succeeded' }));
+          } else {
+            handleServerAppError(res.data, dispatch);
+          }
+        })
+        .catch((error) => {
+          handleServerNetworkError(error, dispatch);
+        });
+    };
 export const logoutTC = (): AppThunk => (dispatch) => {
-  dispatch(appActions.setAppStatus({ status: "loading" }))
+  dispatch(appActions.setAppStatus({ status: 'loading' }));
   authAPI
     .logout()
     .then((res) => {
       if (res.data.resultCode === 0) {
-        dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }))
-        dispatch(appActions.setAppStatus({ status: "succeeded" }))
+        dispatch(authActions.setIsLoggedIn({ isLoggedIn: true }));
+        dispatch(appActions.setAppStatus({ status: 'succeeded' }));
       } else {
-        handleServerAppError(res.data, dispatch)
+        handleServerAppError(res.data, dispatch);
       }
     })
     .catch((error) => {
-      handleServerNetworkError(error, dispatch)
-    })
-}
+      handleServerNetworkError(error, dispatch);
+    });
+};
+
 
 // types
